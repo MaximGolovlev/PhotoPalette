846ae5f874dd0cd381034837b5fbe650281737c8
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-28 12:57:09 +0300

    Initial Commit

---
+++ PhotoPalette/PhotoPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPaletteApp.swift
+//  PhotoPalette
+//
+//  Created by Maxim Golovlev on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
